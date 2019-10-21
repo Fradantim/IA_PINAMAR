2108cb5dd001eeb5d68cb3c22004d5e10aa5e9cd
fradantim <fradantim@pop-os.localdomain>
2019-10-21 12:40:05 -0300

    Se crea entidad Empleado

--- iatmi/iatmi-DAO/src/main/java/com/ia/tmi/iatmi/persistence/entities/Empleado.java
+++ iatmi/iatmi-DAO/src/main/java/com/ia/tmi/iatmi/persistence/entities/Empleado.java
@@ -4,14 +4,40 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Empleado {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer legajo;
 	
 	@Column
 	private Date fechaAlta;
+	
+	
+	public Empleado() {	}
+
+	public Empleado(Date fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}
+
+	public Integer getLegajo() {
+		return legajo;
+	}
+
+	public void setLegajo(Integer legajo) {
+		this.legajo = legajo;
+	}
+
+	public Date getFechaAlta() {
+		return fechaAlta;
+	}
+
+	public void setFechaAlta(Date fechaAlta) {
+		this.fechaAlta = fechaAlta;
+	}
 }
