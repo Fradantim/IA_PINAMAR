40ce7daa392f9b441c71c096d74f8305105f1841
Fradantim <Fradantim@DESKTOP-THABKIC>
2019-10-22 19:51:15 -0300

    jps, agregado de sql nativo

--- iatmi/iatmi-DAO/src/main/java/com/ia/tmi/iatmi/persistence/repository/LiquidacionRepository.java
+++ iatmi/iatmi-DAO/src/main/java/com/ia/tmi/iatmi/persistence/repository/LiquidacionRepository.java
@@ -1,11 +1,23 @@
 package com.ia.tmi.iatmi.persistence.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ia.tmi.iatmi.persistence.entities.Liquidacion;
+import com.ia.tmi.iatmi.persistence.entities.Persona;
 
 @Repository
 public interface LiquidacionRepository extends JpaRepository<Liquidacion, Integer> {
 
+	@Query(value = "select p.* from liquidacion l " + 
+			"	right join persona p on l.empleado_id != p.id " + 
+			"		and year(l.fecha) = :anio " + 
+			"		and month(l.fecha) = :mes " + 
+			"where p.tipo_empleado_id IS NOT NULL", nativeQuery = true)
+	public List<Persona> findAllLiquidacionPersona(@Param("anio") int anio, @Param("mes") int mes);
+	
 }
