b054e36c3b4c487d7d6fde4062b5a9395ed599a8
Alejandro Foglino <alefoglino@gmail.com>
2019-10-23 10:20:05 -0300

    Cambios  filtro consulta liquidacion

--- iatmi/iatmi-DAO/src/main/java/com/ia/tmi/iatmi/persistence/repository/LiquidacionRepository.java
+++ iatmi/iatmi-DAO/src/main/java/com/ia/tmi/iatmi/persistence/repository/LiquidacionRepository.java
@@ -11,12 +11,13 @@ import com.ia.tmi.iatmi.persistence.entities.Liquidacion;
 
 @Repository
 public interface LiquidacionRepository extends JpaRepository<Liquidacion, Integer> {
-
-	@Query(value = "select p.* from liquidacion l " + 
-			"	right join persona p on l.empleado_id != p.id " + 
-			"		and year(l.fecha) = :anio " + 
-			"		and month(l.fecha) = :mes " + 
-			"where p.tipo_empleado_id IS NOT NULL", nativeQuery = true)
+	// @Query(value = "select p.* from liquidacion l " +
+	// " right join persona p on l.empleado_id != p.id " +
+	// " and year(l.fecha) = :anio " +
+	// " and month(l.fecha) = :mes " +
+	// "where p.tipo_empleado_id IS NOT NULL", nativeQuery = true)
+	@Query(value = "select p.id, p.apellido, p.dni, p.email, p.fecha_alta, p.fecha_nacimiento, p.nombre, \r\n"
+			+ "p.sexo, p.sueldo_basico_costo_hora, p.habilitacion_id, p.tipo_empleado_id from  persona p where p.id not in (select l.empleado_id from liquidacion l where  year(l.fecha) = :anio and month(l.fecha) = :mes  ) and p.tipo_empleado_id IS NOT NULL")
 	public List<Object[]> findAllLiquidacionPersona(@Param("anio") int anio, @Param("mes") int mes);
-	
+
 }
