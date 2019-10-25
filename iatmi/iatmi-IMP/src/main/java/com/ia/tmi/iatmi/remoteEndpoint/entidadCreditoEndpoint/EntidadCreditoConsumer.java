e2fce5acd6c3b16ae8bb030ca8f7f3b2029c884b
fradantim <fradantim@pop-os.localdomain>
2019-10-25 10:07:14 -0300

    Fix pago con TD y otras cosas

--- iatmi/iatmi-IMP/src/main/java/com/ia/tmi/iatmi/remoteEndpoint/entidadCreditoEndpoint/EntidadCreditoConsumer.java
+++ iatmi/iatmi-IMP/src/main/java/com/ia/tmi/iatmi/remoteEndpoint/entidadCreditoEndpoint/EntidadCreditoConsumer.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.ia.tmi.iatmi.exception.RemoteEndpointException;
 import com.ia.tmi.iatmi.persistence.entities.Pago;
 import com.ia.tmi.iatmi.remoteEndpoint.EndpointConsumer;
 
@@ -21,10 +22,14 @@ public class EntidadCreditoConsumer extends EndpointConsumer{
 	private static final Logger logger = LoggerFactory.getLogger(EndpointConsumer.class);
 	
 	public void pagarFactura(Pago pago, String cardNumber, String dniCuilCuit, String expirationDate, String securityCode) {
-		EnteCreditoPagarRequest request = new EnteCreditoPagarRequest(pago, cardNumber, dniCuilCuit, CUIL, expirationDate, securityCode);
-		logger.debug("Enviando pago con TC a sistema Ente Credito: "+request);
-		
-		Cuota[] response = getRestTemplate().postForObject(pagarUrl, request, Cuota[].class);
-		logger.debug("Respuesta recibida: "+ response);
+		try {
+			EnteCreditoPagarRequest request = new EnteCreditoPagarRequest(pago, cardNumber, dniCuilCuit, CUIL, expirationDate, securityCode);
+			logger.debug("Enviando pago con TC a sistema Ente Credito: "+request);
+			
+			Cuota[] response = getRestTemplate().postForObject(pagarUrl, request, Cuota[].class);
+			logger.debug("Respuesta recibida: "+ response);
+		} catch (Exception e) {
+			throw new RemoteEndpointException("Error de comunicacion al consumir el servicio del Ente Crediticio. "+e.getMessage());
+		}
 	}
 }
--- iatmi/iatmi-IMP/src/main/java/com/ia/tmi/iatmi/remoteEndpoint/entidadDebitoEndpoint/EntidadDebitoConsumer.java
+++ iatmi/iatmi-IMP/src/main/java/com/ia/tmi/iatmi/remoteEndpoint/entidadDebitoEndpoint/EntidadDebitoConsumer.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.ia.tmi.iatmi.exception.RemoteEndpointException;
 import com.ia.tmi.iatmi.persistence.entities.Pago;
 import com.ia.tmi.iatmi.remoteEndpoint.EndpointConsumer;
 
@@ -20,12 +21,14 @@ public class EntidadDebitoConsumer extends EndpointConsumer{
 	private static final Logger logger = LoggerFactory.getLogger(EndpointConsumer.class);
 	
 	public void pagarFactura(Pago pago, String debitCardNumber, String expirationMonth, String expirationYear, String securityCode) {
-		EntidadDebitoPagarRequest request = new EntidadDebitoPagarRequest(pago, debitCardNumber, securityCode, expirationMonth, expirationYear, CBU);
-		logger.debug("Enviando pago con TD a sistema Ente Debito Banco: "+request);
-		
-		//TODO 0!! HACER TEST DE COBRO CON TD
-		getRestTemplate().postForObject(pagarUrl, request, Object.class);
-		logger.debug("Respuesta recibida: "+ null);
+		try {
+			EntidadDebitoPagarRequest request = new EntidadDebitoPagarRequest(pago, debitCardNumber, securityCode, expirationMonth, expirationYear, CBU);
+			logger.debug("Enviando pago con TD a sistema Ente Debito Banco: "+request);
+			
+			EntidadDebitoPagarResponse response = getRestTemplate().postForObject(pagarUrl, request, EntidadDebitoPagarResponse.class);
+			logger.debug("Respuesta recibida: "+ response);
+		} catch (Exception e) {
+			throw new RemoteEndpointException("Error de comunicacion al consumir el servicio del Banco. "+e.getMessage());
+		}
 	}
-	
 }
